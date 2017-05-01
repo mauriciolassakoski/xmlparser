eb33af0a4a9e8e7aa2b1d3da3c7f51cac1ae1840
Mauricio Lassakoski <mauriciolassakoski@gmail.com>
2017-04-30 21:13:45 -0300

    initial commit

---
+++ src/main/java/br/com/mlassakoski/XmlparserApplication.java
@@ -0,0 +1,12 @@
+package br.com.mlassakoski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlparserApplication.class, args);
+	}
+}
